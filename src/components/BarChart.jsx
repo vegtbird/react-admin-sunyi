20c2ff0faac7f89417a9c748c89f34fe4c5f2129
sunyi <18844544602@163.com>
2022-12-13 10:03:25 +0800

    Add BarChart and Bar components

---
+++ src/components/BarChart.jsx
@@ -0,0 +1,171 @@
+import { useTheme } from '@mui/material'
+        // install (please make sure versions match peerDependencies)
+// yarn add @nivo/core @nivo/bar
+import { ResponsiveBar } from '@nivo/bar'
+
+// make sure parent container have a defined height when using
+// responsive component, otherwise height will be 0 and
+// no chart will be rendered.
+// website examples showcase many properties,
+// you'll often use just a few of them.
+import { mockBarData as data } from '../data/mockData'
+import { tokens } from "../theme";
+
+// 默认不在Dashboard中展示，如果在Dashboard中展示，不显示坐标轴文字
+// 箭头函数传参写成( isDashboard = false )为什么不行？？
+// ---React函数组件只能接收一个参数props，且props为对象
+const BarChart = ({ isDashboard = false }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+        <ResponsiveBar
+            // data:object[],required
+            data={ data }
+            //设置主题及样式
+            theme={{
+                axis: {
+                    domain: {
+                      line: {
+                        stroke: colors.grey[100],
+                      },
+                    },
+                    legend: {
+                      text: {
+                        fill: colors.grey[100],
+                      },
+                    },
+                    ticks: {
+                      line: {
+                        stroke: colors.grey[100],
+                        strokeWidth: 1,
+                      },
+                      text: {
+                        fill: colors.grey[100],
+                      },
+                    },
+                  },
+                legends: {
+                    text: {
+                        fill: colors.grey[100]
+                    }
+                }
+            }}
+            keys={[
+                'hot dog',
+                'burger',
+                'sandwich',
+                'kebab',
+                'fries',
+                'donut'
+            ]}
+            indexBy="country"
+            margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+            padding={0.3}
+            valueScale={{ type: 'linear' }}
+            indexScale={{ type: 'band', round: true }}
+            colors={{ scheme: 'nivo' }}
+            defs={[
+                {
+                    id: 'dots',
+                    type: 'patternDots',
+                    background: 'inherit',
+                    color: '#38bcb2',
+                    size: 4,
+                    padding: 1,
+                    stagger: true
+                },
+                {
+                    id: 'lines',
+                    type: 'patternLines',
+                    background: 'inherit',
+                    color: '#eed312',
+                    rotation: -45,
+                    lineWidth: 6,
+                    spacing: 10
+                }
+            ]}
+            // fill={[
+            //     {
+            //         match: {
+            //             id: 'fries'
+            //         },
+            //         id: 'dots'
+            //     },
+            //     {
+            //         match: {
+            //             id: 'sandwich'
+            //         },
+            //         id: 'lines'
+            //     }
+            // ]}
+            borderColor={{
+                from: 'color',
+                modifiers: [
+                    [
+                        'darker',
+                        1.6
+                    ]
+                ]
+            }}
+            axisTop={null}
+            axisRight={null}
+            axisBottom={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: isDashboard ? undefined : 'country', // changed
+                legendPosition: 'middle',
+                legendOffset: 32
+            }}
+            axisLeft={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: isDashboard ? undefined :'food', // changed
+                legendPosition: 'middle',
+                legendOffset: -40
+            }}
+            enableLabel={false} // changed,取消Bar上的文字标记
+            labelSkipWidth={12}
+            labelSkipHeight={12}
+            labelTextColor={{
+                from: 'color',
+                modifiers: [
+                    [
+                        'darker',
+                        1.6
+                    ]
+                ]
+            }}
+            legends={[
+                {
+                    dataFrom: 'keys',
+                    anchor: 'bottom-right',
+                    direction: 'column',
+                    justify: false,
+                    translateX: 120,
+                    translateY: 0,
+                    itemsSpacing: 2,
+                    itemWidth: 100,
+                    itemHeight: 20,
+                    itemDirection: 'left-to-right',
+                    itemOpacity: 0.85,
+                    symbolSize: 20,
+                    effects: [
+                        {
+                            on: 'hover',
+                            style: {
+                                itemOpacity: 1
+                            }
+                        }
+                    ]
+                }
+            ]}
+            role="application"
+            ariaLabel="Nivo bar chart demo"
+            barAriaLabel={function(e){return e.id+": "+e.formattedValue+" in country: "+e.indexValue}}
+        />
+  )
+}
+
+export default BarChart
--- src/scenes/bar/index.jsx
+++ src/scenes/bar/index.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { Box } from '@mui/material'
+import Header from '../../components/Header'
+import BarChart from '../../components/BarChart'
 
 const Bar = () => {
   return (
-    <div>Bar</div>
+    <Box m="20px">
+        <Header title="Bar Chart" subtitle="Simple Bar Chart"/>
+        {/* 要显示Bar Chart，其父元素即下面的Box组件必须要设置height属性值 */}
+        <Box height="75vh">
+          <BarChart/>
+        </Box>   
+    </Box>
   )
 }
 
